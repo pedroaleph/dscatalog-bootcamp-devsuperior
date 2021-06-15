20653fc0a60bc5d62a402646def977682d29a8e0
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-06-15 16:12:44 -0400

    Add toast when save product

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,17 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './core/assets/styles/custom.scss'
 import './App.scss';
 import Routes from './Routes';
+import React from 'react';
 const App = () => {
-    
+
 
     return (
-        <Routes />
+        <>
+            <ToastContainer />
+            <Routes />
+        </>
     )
 }
 export default App;

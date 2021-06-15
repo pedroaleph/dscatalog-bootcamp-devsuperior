f3450eb817335beb78b62dd19417fd105411aa7e
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-06-15 19:51:00 -0400

    Finish admin product card layout

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router'
+import Card from '../Card';
 import './styles.scss'
 
 const List = () => {
@@ -12,6 +13,11 @@ const List = () => {
             <button className="btn btn-primary btn-lg" onClick={handleCreate}>
                 ADICIONAR
             </button>
+            <div className="admin-list-container">
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     )
 }
